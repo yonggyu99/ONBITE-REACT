a72abe946376a0c5dfbd5329ed13bc33a79b76fd
박용규 <yoyo5411@naver.com>
2025-04-22 16:36:05 +0900

    ~04.22

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,16 @@
 const Button = ({ children, text, color = "black" }) => {
+  //이벤트 객체체
+  const OnClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={OnClickButton}
+      // onMouseEnter={OnClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
