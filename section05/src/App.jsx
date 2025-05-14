c9631d5c10457faa909200bfafa2da0166319426
박용규 <yoyo5411@naver.com>
2025-05-14 13:34:14 +0900

    04.~~~

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,25 +1,31 @@
-import React from "react";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
 
   return (
     <div>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-      </Button>
+      <h1>{light}</h1>
+      <button
+        onClick={() => {
+          setLight(light === "ON" ? "OFF" : "ON");
+        }}
+      >
+        {light === "ON" ? "끄기" : "켜기"}
+      </button>
+
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </div>
   );
 };
